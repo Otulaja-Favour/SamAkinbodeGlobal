0149830823f58a3a36db5299dceb0be7bace5ef6
Otulaja Favour <otulajafavour14@gmail.com>
2025-06-03 23:46:32 +0100

    imported axios

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import axios from 'axios'
 import App from './App.vue'
 import router from './router'
 
@@ -10,5 +10,7 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+app.config.globalProperties.$axios = axios
 
 app.mount('#app')
