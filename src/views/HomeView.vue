eb3c3ae79e9a6fb2c5eab63c5468355c871c4720
Otulaja Favour <otulajafavour14@gmail.com>
2025-06-08 14:20:20 +0100

    added images for home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,19 +3,47 @@
     <div class="landingPage">
       <transition name="slide-left">
         <div class="imageAspect" v-if="showPage">
-          <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+         
+
+
+          <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="../assets/images/JARIPEO.jpeg" class="d-block w-100" alt="...">
+      <img src="../assets/images/jas.jpeg" class="d-block w-100" alt="...">
+
+    </div>
+  
+    <div class="carousel-item">
+      <img src="../assets/images/jag.jpeg" class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
-      <img src="../assets/images/ja" class="d-block w-100" alt="...">
+      <img src="../assets/images/jaj.jpeg" class="d-block w-100" alt="...">
     </div>
+    
     <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
+      <img src="../assets/images/jay.jpeg" class="d-block w-100" alt="...">
     </div>
+    
   </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
 </div>
+
+
+
+
+
         </div>
       </transition>
 
